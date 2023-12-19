226f0af332eda776aec133c567c2a181f513c91f
ayabass820 <145125448+ayabass820@users.noreply.github.com>
2023-12-19 22:19:59 +0900

    javascript-basic-kadaiにkadai_mdn_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,5 @@
+var dateName = new Date([2023,12,19]);
+  var year = dateName.getFullYear();
+  var month = dateName.getMonth() +1;
+  var day = dateName.getDate();
+console.log(year + "年" + month + "月" + day + "日");
