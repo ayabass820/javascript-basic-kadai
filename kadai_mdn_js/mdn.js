3dc70961e541f936d774da9b8a070040290546e1
ayabass820 <145125448+ayabass820@users.noreply.github.com>
2023-12-20 21:57:07 +0900

    kadai_mdn_jsを削除

--- kadai_mdn_js/mdn.js
+++
@@ -1,5 +0,0 @@
-var dateName = new Date([2023,12,19]);
-  var year = dateName.getFullYear();
-  var month = dateName.getMonth() +1;
-  var day = dateName.getDate();
-console.log(year + "年" + month + "月" + day + "日");
